921f6bbbbd0136f75486ea8ac6ab1a0736123196
SaeedNMosleh <you@example.com>
2025-01-08 20:16:01 +0100

    ArrayBasedDB

--- index.js
+++ index.js
@@ -3,40 +3,48 @@ Two API Endpoints are created in this file to load and save the data for leaderb
 */
 
 import express from 'express';
-import fs from 'fs';
 
 const app = express();
-const port = 3000;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+// In-memory array to store leaderboard data
+let leaderboardData = [
+    {"name": "John", "score": 42},
+    {"name": "Jane", "score": 35},
+    {"name": "Jim", "score": 30},
+    {"name": "Jill", "score": 25},
+    {"name": "Jack", "score": 20},
+];
+
+// Function to get leaderboard data
 const getLeaderboardData = () => {
-    // Load the leaderboard data from the json file and return it
-    const data = fs.readFileSync('leaderBoardlist.json', 'utf8');
-    return JSON.parse(data);
+    return leaderboardData;
 };
 
-const saveLeaderboardData = (leaderboardData) => {
-    // Save the leaderboard data to the json file
-    fs.writeFileSync('leaderBoardlist.json', JSON.stringify(leaderboardData));
-}
+// Function to save leaderboard data
+const saveLeaderboardData = (data) => {
+    leaderboardData = data;
+};
 
 app.get('/getleaderboard', (req, res) => {
-    // Load the leaderboard data from the json file
-    const leaderboardData = getLeaderboardData();
-    res.json(leaderboardData);
-  });
+    // Load the leaderboard data from the in-memory array
+    const data = getLeaderboardData();
+    res.json(data);
+});
+
+app.get('/', (req, res) => {
+    
+    res.send("SyntaxSprint Backend API is running successfully");
+});
 
 app.post('/saveleaderboard', (req, res) => {
-    // Save the leaderboard data to the json file
-    const leaderboardData = req.body;
-    saveLeaderboardData(leaderboardData);
+    // Save the leaderboard data to the in-memory array
+    const data = req.body;
+    saveLeaderboardData(data);
     res.send('Leaderboard data saved successfully');
 });
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-  })
-
-  export default app;
+// Export the app for Vercel
+export default app;
